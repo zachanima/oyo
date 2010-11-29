86903a880aaf66c866e87b55b8591ff060f824d8
Zach Ewert <zachanima@gmail.com>
2010-11-29 07:47:57 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oyo::Application.config.secret_token = '99f9003d914af79725da49df8651aa964af0c4660f2fad3e7c1fb9c13a1fd3e2e4c2258d4ac7cfb1bfc978bd176039723f2e8d09c7dcbe4a7eefac432fe08790'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Oyo::Application.config.session_store :cookie_store, :key => '_oyo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Oyo::Application.config.session_store :active_record_store
