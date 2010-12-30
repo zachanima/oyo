66c644f58483fbea8705302f9c2939d692f38aae
Zach Ewert <zachanima@gmail.com>
2010-12-30 19:38:31 +0100

    Add link, unlink button to tiny mce controls

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,5 +1,9 @@
 class TextsController < ElementsController
   uses_tiny_mce :only => [:new, :edit], :options => {
-    :content_css => '/stylesheets/style.css'
+    :content_css => '/stylesheets/style.css',
+    :theme => 'advanced',
+    :theme_advanced_buttons1 => %w[bold italic strikethrough separator bullist numlist separator undo redo separator link unlink] * ',',
+    :theme_advanced_buttons2 => '',
+    :theme_advanced_buttons3 => ''
   }
 end
