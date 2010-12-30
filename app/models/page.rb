5c85cb46b7549cf17a79b9e201bb656465b911cc
Zach Ewert <zachanima@gmail.com>
2010-12-30 21:04:10 +0100

    Add parent id to links, galleries forms

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
   belongs_to :page, :primary_key => :parent_id
-  has_many :pages, :primary_key => :parent_id
+  has_many :pages, :primary_key => :parent_id, :dependent => :destroy
   has_many :elements, :dependent => :destroy
 
   attr_accessible :title, :url, :parent_id
